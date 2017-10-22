d322cd1c029642a1d42fa59a8a7b16efa911f71d
vfabrichnov <zorumnipa@gmail.com>
2017-10-22 04:19:17 +0300

    Дропдаун к экшонам

--- src/App.js
+++ src/App.js
@@ -5,56 +5,16 @@ import './App.css';
 import axios from 'axios';
 import {
   Table,
-  // Button,
+  Button,
   // Label,
   Glyphicon,
   Panel,
   Accordion,
+  DropdownButton,
+  MenuItem,
 } from 'react-bootstrap';
 import Metric from "./Metric";
 
-// const servers = [{
-//   'id': '442d3f41-7204-477d-bc9e-4de6c55717bd',
-//   'serverName': 'Deidre',
-//   'tags': [],
-//   'projectName': 'Freya',
-//   'region_name': 'ru-1',
-//   'image_id': '210f6963-58c0-4607-b657-887597fb90fa',
-//   'description': 'Deidre',
-//   'flavor_id': 'f3528e1a-973a-4128-b961-ea13d4ce07f7',
-//   'ip': '192.168.0.4',
-//   'volume_id': '5b567559-ebf2-438e-bca4-0eb3f914c795'
-// }, {
-//   'id': 'f883c690-a797-4de7-84a8-846058296560',
-//   'serverName': 'Catherine',
-//   'tags': [],
-//   'projectName': 'it_works',
-//   'region_name': 'ru-1',
-//   'image_id': '210f6963-58c0-4607-b657-887597fb90fa',
-//   'description': 'Catherine',
-//   'flavor_id': '051d0688-5dfc-41e1-bfec-0eefad0b4e33',
-//   'ip': '192.168.0.4',
-//   'volume_id': 'eb151d58-b238-457f-a05b-9e605898ec49'
-// }, {
-//   'id': 'f6a56cf4-d02a-49c7-a555-1b887ac96df9',
-//   'serverName': 'Vitalik',
-//   'tags': ['asd', 'jjj', 'tes8t'],
-//   'projectName': 'it_works',
-//   'region_name': 'ru-1',
-//   'image_id': '210f6963-58c0-4607-b657-887597fb90fa',
-//   'description': 'Fuking good Server',
-//   'flavor_id': '0487332d-ac86-4c8f-a05c-b068c21e68f8',
-//   'ip': '77.244.216.218',
-//   'volume_id': '52679d68-a2f6-4da7-baa4-ed23dae3ba5c'
-// }];
-//
-// const serverDetail = {
-//   'id': 'f6a56cf4-d02a-49c7-a555-1b887ac96df9',
-//   'ram': 512,
-//   'cpus': 1,
-//   'os': 'Ubuntu 16.04 LTS 64-bit',
-//   'volume': {'size': 10240, 'type': 'fast'}
-// };
 
 class Panels extends Component {
   constructor(props) {
@@ -65,9 +25,16 @@ class Panels extends Component {
     };
   }
   getServerDetail() {
-    axios.post('https://itjustworks.me/servers/detail/?viewer_id=1', this.props.server)
+    axios.post('https://itjustworks.me:8443/servers/detail/?viewer_id=1', this.props.server)
       .then((response) => {
-        this.setState({serverDetail: response.data});
+      let qwer = Object.assign({}, response.data, this.props.server);
+        this.setState({serverDetail: qwer});
+      })
+  }
+  runAction(actionName){
+    axios.post('https://itjustworks.me:8443/servers/' + this.props.server.id + '/' + actionName, {'project_name': this.props.server.project_name})
+      .then((response) => {
+        console.log('Done')
       })
   }
 
@@ -76,7 +43,7 @@ class Panels extends Component {
 
     return (
       <div>
-        <Panel collapsible header={
+        <Panel collapsible header={<div>
           <h4 onClick={() => {this.setState({showDetail: !this.state.showDetail}); this.getServerDetail()} }>
             <Glyphicon
               title={server.status}
@@ -85,6 +52,15 @@ class Panels extends Component {
             /> {server.serverName}
             <small>({server.tags.join(', ')})</small>
           </h4>
+          <DropdownButton bsStyle='Danger' title='Actions'>
+            <MenuItem eventKey="1" onClick={this.runAction('reboot/hard/')}>Hard Reboot</MenuItem>
+            <MenuItem eventKey="2">Soft Reboot</MenuItem>
+            <MenuItem eventKey="3">Pause</MenuItem>
+            <MenuItem eventKey="4">Unpause</MenuItem>
+            <MenuItem eventKey="5">Start</MenuItem>
+            <MenuItem eventKey="6">Stop</MenuItem>
+          </DropdownButton>
+        </div>
         }>
           {this.state.showDetail && <ServerTable serverDetail={this.state.serverDetail}/>}
         </Panel>
@@ -101,15 +77,15 @@ class ServerTable extends Component {
         <tbody>
         <tr>
           <td><Glyphicon glyph='tag'>{serverDetail.id}</Glyphicon></td>
-          <td>{serverDetail.region}</td>
+          <td>{serverDetail.region_name}</td>
         </tr>
         <tr>
           <td>{serverDetail.os}</td>
-          <td>{serverDetail.dataCenter}</td>
+          <td></td>
         </tr>
         <tr>
           <td>{serverDetail.cpus} vCPU, Память {serverDetail.ram} МБ</td>
-          <td>{serverDetail.addr}</td>
+          <td></td>
         </tr>
         </tbody>
       </Table>
@@ -130,7 +106,7 @@ class App extends Component {
   }
 
   getServers() {
-    axios.get('https://itjustworks.me/servers/?viewer_id=1')
+    axios.get('https://itjustworks.me:8443/servers/?viewer_id=1')
     .then((response) => {
       this.setState({servers: response.data});
     })
